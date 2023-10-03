3c9750052febc3a17ea90e5ef94edc957f883af5
Erik Wessman <erikwessman10@gmail.com>
2023-10-03 13:44:08 +0200

    Change to button

--- src/App.tsx
+++ src/App.tsx
@@ -42,20 +42,20 @@ export default function App() {
     <div>
       <span>The WebSocket is currently {connectionStatus}</span>
       <div style={{ display: "flex" }}>
-        <div onMouseDown={(event) => HandleButtonDown(event, DirectionState.LEFT)}
+        <button onMouseDown={(event) => HandleButtonDown(event, DirectionState.LEFT)}
           onMouseUp={(event) => HandleButtonUp(event)}
           onTouchStart={(event) => HandleButtonDown(event, DirectionState.LEFT)}
           onTouchEnd={(event) => HandleButtonUp(event)}
           onTouchCancel={(event) => HandleButtonUp(event)}>
           <img src="svg/left_arrow.svg" alt="Left arrow" />
-        </div>
-        <div onMouseDown={(event) => HandleButtonDown(event, DirectionState.RIGHT)}
+        </button>
+        <button onMouseDown={(event) => HandleButtonDown(event, DirectionState.RIGHT)}
           onMouseUp={(event) => HandleButtonUp(event)}
           onTouchStart={(event) => HandleButtonDown(event, DirectionState.RIGHT)}
           onTouchEnd={(event) => HandleButtonUp(event)}
           onTouchCancel={(event) => HandleButtonUp(event)}>
           <img src="svg/right_arrow.svg" alt="Right arrow" />
-        </div>
+        </button>
       </div>
     </div>
   );
