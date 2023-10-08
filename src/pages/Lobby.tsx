76a249ba0c7d74745ec24001d0bcad16a14f9b8f
Erik Wessman <erikwessman10@gmail.com>
2023-10-08 22:56:57 +0200

    use navigate in lobby

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 export default function Lobby() {
+    const [inputText, setInputText] = useState<string>("");
+    const navigate = useNavigate();
+
+    function HandleClickJoinGame() {
+        navigate(inputText);
+    }
+
     return (
         <div>
             <div>
                 <p>This is the lobby!</p>
             </div>
             <div>
-                <input type="text" placeholder="Insert lobby code" />
+                <input type="text" placeholder="Insert lobby code" value={inputText} onChange={(e) => setInputText(e.target.value)} />
             </div>
             <div>
-                <button>
+                <button onClick={HandleClickJoinGame}>
                     Join game
                 </button>
             </div>
