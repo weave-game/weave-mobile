b4fe54a70cc8d5ceb830ae383ac5969ae93933e0
Erik Wessman <erikwessman10@gmail.com>
2023-10-19 17:22:07 +0200

    Trying fastclick

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './Router';
+import FastClick from 'fastclick';
+
+if ('addEventListener' in document) {
+  document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+      (FastClick as any).attach(document.body);
+    },
+    false,
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <Router />
+  <Router />
 );
