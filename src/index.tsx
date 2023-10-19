9a9d10ebf84cf527a45bdce1e4bea34a8db74c95
Erik Wessman <erikwessman10@gmail.com>
2023-10-19 17:23:46 +0200

    removed fastclick

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './Router';
-import FastClick from 'fastclick';
-
-if ('addEventListener' in document) {
-  document.addEventListener(
-    'DOMContentLoaded',
-    () => {
-      (FastClick as any).attach(document.body);
-    },
-    false,
-  );
-}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
